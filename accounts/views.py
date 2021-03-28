89abfe86f040a50d9955932db5b956724f98397e
Alex Berndt <berndtae@gmail.com>
2021-03-28 10:28:28 +0200

    Login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+def signup_view(request):
+
+    if request.method == 'POST':
+        # validate data 
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            return redirect('articles:list')
+
+    else:
+
+        form = UserCreationForm()
+        
+    data = {"form": form}
+    return render(request, 'accounts/signup.html', data)
+
+
+def login_view(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            user = form.get_user()
+            login(request, user)
+            
+            return redirect('articles:list')       
+    else:
+        form = AuthenticationForm()
+
+    data = {"form": form}
+    return render(request, 'accounts/login.html', data)
+
+
+def logout_view(request):
+
+    # logging out using a POST request
+    if request.method == 'POST':
+        logout(request)
+
+        return redirect('articles:list')
--- ml_webapp/urls.py
+++ ml_webapp/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
+    re_path(r'^accounts/', include('accounts.urls')),
     re_path(r'^articles/', include('articles.urls')),
     re_path(r'^about/$', views.about),
     re_path(r'^$', views.homepage),
